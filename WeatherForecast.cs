be0f79ebe9562ca307cc71d2bd76095d0e5cffea
Shakthi <lonedagger1988@gmail.com>
2022-10-27 16:14:55 +0530

    comment branch

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -7,6 +7,7 @@ namespace MovieAPP
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
+        //hell this shakthi branch
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
